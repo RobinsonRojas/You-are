f98ec5cb9c8beeee44d2ecc40a5a560f28698a4e
Robinson Rojas <totorox123@gmail.com>
2022-01-31 20:31:21 -0500

    parrafo y log

--- js.js
+++ js.js
@@ -8,12 +8,13 @@ async function country() {
   if (!res.ok) {
     let error = "name no fount";
     alert(error);
+    throw new Error(error);
   }
 
   return resJSON;
 }
 
 function show(n) {
-  console.log("n : ");
+  console.log("n : " + n.country[0].country_id);
   document.getElementById("p").innerHTML = n.country[0].country_id;
 }
